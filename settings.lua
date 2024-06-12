4e51f4ca372bb1c5fc907fb1b821fdc56068bc52
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-06-12 08:26:48 +0200

    v1.2.21

--- settings.lua
+++ settings.lua
@@ -13,7 +13,7 @@ function HealerProtection:ToggleSettings()
 end
 
 function HealerProtection:InitSetting()
-	D4:SetVersion(AddonName, 135923, "1.2.20")
+	D4:SetVersion(AddonName, 135923, "1.2.21")
 	HPTABPC["MMBTNTAB"] = HPTABPC["MMBTNTAB"] or {}
 	if HPTABPC["MMBTN"] == nil then
 		HPTABPC["MMBTN"] = true
@@ -45,7 +45,7 @@ function HealerProtection:InitSetting()
 			["pTab"] = {"CENTER"},
 			["sw"] = 520,
 			["sh"] = 520,
-			["title"] = format("HealerProtection |T135923:16:16:0:0|t by |cff3FC7EBD4KiR |T132115:16:16:0:0|t v|cff3FC7EB%s", "1.2.20")
+			["title"] = format("HealerProtection |T135923:16:16:0:0|t by |cff3FC7EBD4KiR |T132115:16:16:0:0|t v|cff3FC7EB%s", "1.2.21")
 		}
 	)
 
